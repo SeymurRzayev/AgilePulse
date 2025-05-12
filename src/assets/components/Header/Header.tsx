a9be71036b66bc0824d075598b02e143e5af4ada
Aytan <aytanumudova@gmail.com>
2025-05-12 14:57:05 +0400

    initializing header

---
+++ src/assets/components/Header/Header.tsx
@@ -0,0 +1,47 @@
+import "./Header.scss";
+import logo from "../../logo.svg";
+
+const Header = () => {
+  return (
+    <div className="container">
+      <header>
+        {/* Logo */}
+        <div className="logo">
+          <img src={logo} alt="Agile Pulse logo" />
+        </div>
+        {/* Navigation */}
+        <nav>
+          <div className="nav-left">
+            <ul>
+              <li>
+                <a href="#">Ana səhifə</a>
+              </li>
+              <li>
+                <a href="#">Təlimlər</a>
+              </li>
+              <li>
+                <a href="#">İmkanlar</a>
+              </li>
+              <li>
+                <a href="#">Haqqımızda</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="nav-right">
+            <ul>
+              <li>
+                <a href="#">Qeydiyyat</a>
+              </li>
+              <li>
+                <a href="#">Daxil ol</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
