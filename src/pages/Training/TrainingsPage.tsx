495fe9da84f66100ff89c9a52b880f01aad345c5
Nurlan Məmmədli <nrlmnnn532@gmail.com>
2025-05-14 19:11:31 +0400

    İnput, icons & Size

--- src/pages/Training/TrainingsPage.tsx
+++ src/pages/Training/TrainingsPage.tsx
@@ -144,33 +144,35 @@ const TrainingsPage = () => {
         </div>
       </div>
 
-      <div className={styles.searchWrapper}>
-        <div className={styles.searchContainer}>
-          <div className={styles.searchBar}>
-            <input type="text" placeholder="Search" />
-            <svg className={styles.searchIcon} width="24" height="24" viewBox="0 0 24 24" fill="none">
-              <path
-                d="M21 21L16.65 16.65M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z"
-                stroke="#222222"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </div>
-          <button className={styles.filterButton}>
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-              <path
-                d="M3 6H21M6 12H18M10 18H14"
-                stroke="#222222"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </button>
-        </div>
-      </div>
+<div className={styles.searchWrapper}>
+  <div className={styles.searchContainer}>
+    <div className={styles.searchBar}>
+      <input type="text" placeholder="Search" />
+      <button className={styles.searchButton}>
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+          <path
+            d="M21 21L16.65 16.65M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z"
+            stroke="#222222"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </button>
+    </div>
+  </div>
+  <button className={styles.filterButton}>
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <path
+        d="M3 6H21M6 12H18M10 18H14"
+        stroke="#222222"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  </button>
+</div>
 
       <div className={styles.categoriesContainer}>
         <ul className={styles.navigation}>
