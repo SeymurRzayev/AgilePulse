19345f4dbc54a54daa0ee10562a377c2674c6898
mammadovailaha <ilamemmedova14@gmail.com>
2025-05-14 17:15:38 +0400

    feat: add Banner (responsive) and TestSection components

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,1 +1,15 @@
 
+import type { FC } from "react";
+import { Banner } from "../../components/Banner/Banner";
+import styles from "./Home.module.css";
+import { Test } from "../../components/TestSection/Test";
+export const Home:FC=()=>{
+    return(
+        <div className={styles.HomeSection}>
+             <Banner/>
+             <Test/>
+        </div>
+       
+        
+    )
+}
