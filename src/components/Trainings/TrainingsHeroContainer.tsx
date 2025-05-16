94a7d5ca4a6e152b70bf468e2e6aa0d9281d7914
Sedi98 <sedi9816@gmail.com>
2025-05-16 22:43:28 +0400

    Trainings page is fully refactored. Check full code please

---
+++ src/components/Trainings/TrainingsHeroContainer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../../assets/styles/Trainings.module.css";
+
+const TrainingsHeroContainer: React.FC = () => {
+  return (
+    <div className={styles.heroSection}>
+      <div className={styles.heroContent}></div>
+    </div>
+  );
+};
+
+export default TrainingsHeroContainer;
