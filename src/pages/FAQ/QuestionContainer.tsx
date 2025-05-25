9a670840f72eddb3bef5af0ce5567676429c665a
mammadovailaha <ilamemmedova14@gmail.com>
2025-05-25 18:22:37 +0400

    FAQ responsive design

--- src/pages/FAQ/QuestionContainer.tsx
+++ src/pages/FAQ/QuestionContainer.tsx
@@ -11,25 +11,27 @@ const QuestionContainer: React.FC<QuestionContainerProps> = ({
 }) => {
   return (
     <div
-      className="w-[95%] max-w-[900px] bg-[#28448D] rounded-[15px] flex justify-end items-center relative mx-auto"
+      className="w-[95%] max-w-[900px] bg-[#28448D] rounded-[15px] flex justify-end items-center relative mx-auto overflow-hidden"
       style={{
         boxShadow: "-1px 10px 26px -8px rgba(0,0,0,0.2)",
       }}
     >
+      {/* Ağ div: sağ radius var, sol radius yoxdur */}
       <div
-        className="w-[97.5%] bg-white rounded-r-[15px] h-full py-6 pl-4 sm:pl-6 sm:mr-[20px] md:pl-8"
+        className="w-[97.5%] bg-white rounded-r-[15px] rounded-l-none h-full py-6 pl-6  pr-[60px]"
         style={{
           boxShadow: "4px 4px 12px 5px rgba(0, 0, 0, 0.09)",
         }}
       >
-        <p className="font-bold text-base  sm:text-base md:text-xl lg:text-2xl leading-6 sm:leading-7 md:leading-8 text-[#2C4B9B] font-corbel ">
+        <p className="font-bold text-base sm:text-base md:text-xl lg:text-2xl leading-6 sm:leading-7 md:leading-8 text-[#2C4B9B] font-corbel">
           {question}
         </p>
       </div>
 
+      {/* Dairə klik üçün */}
       <div
         onClick={openAnswer}
-        className="w-[40px] h-[40px] sm:w-[48px] sm:h-[48px] md:w-[52px] md:h-[52px] absolute right-3 flex justify-center items-center z-10"
+        className="w-[40px] h-[40px] sm:w-[48px] sm:h-[48px] md:w-[52px] md:h-[52px] absolute right-3 flex justify-center items-center z-10 cursor-pointer"
       >
         <img
           src={circleClose}
