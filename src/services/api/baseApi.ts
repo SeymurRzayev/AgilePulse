0ed074dab35c541c3602449da1e1de384f7639f3
Farmayil001 <gasanoff8085@gmail.com>
2025-05-27 00:13:44 +0400

    Api structure

---
+++ src/services/api/baseApi.ts
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { base_url } from '../../config/adminApi';
+
+
+export const baseApi = createApi({
+    reducerPath: 'api',
+    baseQuery: fetchBaseQuery({
+        baseUrl: base_url,
+        prepareHeaders: (headers) => {
+            const token = localStorage.getItem('token');
+            if (token) {
+                headers.set('Authorization', `Bearer ${token}`);
+            }
+            return headers;
+        },
+    }),
+    endpoints: () => ({}),
+});
