ecbeca34ac2ed3a430c3eb0b8863005cd1e7cd17
Nurlan Məmmədli <nrlmnnn532@gmail.com>
2025-05-30 15:08:59 +0400

    Fixed contactUsSchema.ts and TrainingsContactUs.tsx

--- src/validation/contactUsSchema.ts
+++ src/validation/contactUsSchema.ts
@@ -1,8 +1,22 @@
 import * as yup from "yup";
-import { emailField, messageSchema, nameSchema } from "./validationSchema";
+
+export const nameSchema = yup
+  .string()
+  .required("Ad daxil edilməlidir.")
+.matches(/^[A-Za-zƏəÖöÜüĞğÇçŞşİı\s]+$/, "Ad və soyad yalnız hərflərdən ibarət olmalıdır")
+
+export const emailField = yup
+  .string()
+  .required("Email daxil edilməlidir.")
+  .email("Email formatı düzgün deyil");
+
+export const messageSchema = yup
+  .string()
+  .required("Mesaj daxil edilməlidir.")
+  .min(10, "Mesaj çox qısadır. Ən azı 10 simvol olmalıdır.");
 
 export const contactUsSchema = yup.object({
-  message: messageSchema,
   name: nameSchema,
   email: emailField,
+  message: messageSchema,
 });
