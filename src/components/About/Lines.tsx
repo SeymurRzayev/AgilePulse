11b42d338db5a27306e146146f5cf27b80cd365d
Khadija Bayramova <safarlikhadija98@gmail.com>
2025-06-01 12:52:31 +0400

    design changings on Team component

--- src/components/About/Lines.tsx
+++ src/components/About/Lines.tsx
@@ -12,103 +12,103 @@ function Lines() {
     <>
       {/* up left line */}
       <img
-        className="absolute  hidden  md:max-2xl:block lg:block  w-[30.2%]  top-[4%] left-[20%] "
+        className="absolute  hidden  lg:max-2xl:block lg:block  w-[30.2%]  top-[4%] left-[20%] "
         src={lineteamsvg}
         alt=""
       />
       {/* up right line */}
       <img
-        className="absolute  hidden  md:max-2xl:block lg:block w-[30.2%] top-[4%] right-[20%] "
+        className="absolute  hidden   lg:block w-[30.2%] top-[4%] right-[20%] "
         src={lineteamsvg}
         alt="line"
       />
       {/* out right line */}
       <img
-        className="absolute  hidden  md:max-2xl:block lg:block h-[19.4%]  top-[4%] right-[19.5%] "
+        className="absolute  hidden   lg:block h-[19.4%]  top-[4%] right-[19.5%] "
         src={arrowlinesvg}
         alt="line"
       />
       {/* out left line */}
       <img
-        className="absolute  hidden  md:max-2xl:block lg:block h-[19.4%]   top-[4%] left-[19.9%] "
+        className="absolute  hidden   lg:block h-[19.4%]   top-[4%] left-[19.9%] "
         src={arrowlinesvg}
         alt="line"
       />
       {/* inner up downward arrow */}
       <img
-        className="absolute hidden  md:max-2xl:block lg:block h-[3.6%] top-[4%] right-[50%] "
+        className="absolute hidden   lg:block h-[3.6%] top-[4%] right-[50%] "
         src={arrshortteam}
         alt="line"
       />
       {/* inner up line */}
       <img
-        className="absolute  hidden  md:max-2xl:block lg:block  w-[30.2%]  top-[5.5%] right-[35.4%] "
+        className="absolute  hidden   lg:block  w-[30.2%]  top-[5.5%] right-[35.4%] "
         src={lineteamsvg}
         alt="line"
       />
       {/* inner left line */}
       <img
-        className="absolute  hidden  md:max-2xl:block lg:block h-[19.68%]  top-[5.5%] left-[34.8%] "
+        className="absolute  hidden   lg:block h-[19.68%]  top-[5.5%] left-[34.8%] "
         src={shortsideline}
         alt="line"
       />
       {/* inner right line */}
       <img
-        className="absolute  hidden  md:max-2xl:block lg:block h-[42%]  top-[5.5%] right-[35.4%] "
+        className="absolute  hidden   lg:block h-[42%]  top-[5.5%] right-[35.4%] "
         src={longsideline}
         alt="line"
       />
       {/* inner bottom line */}
       <img
-        className="absolute  hidden  md:max-2xl:block lg:block w-[15%] top-[24.9%] left-[34.8%] "
+        className="absolute  hidden   lg:block w-[15%] top-[24.9%] left-[34.8%] "
         src={shortlineteam}
         alt="line"
       />
       {/* inner downward arrow */}
       <img
-        className="absolute hidden  md:max-2xl:block lg:block h-[5%] top-[24.9%] left-[48.7%] "
+        className="absolute hidden   lg:block h-[5%] top-[24.9%] left-[48.7%] "
         src={arrowline152}
         alt="line"
       />
       {/* second bottom line */}
       <img
-        className="absolute hidden  md:max-2xl:block lg:block w-[14.8%]  top-[47.1%] right-[34.9%] "
+        className="absolute hidden   lg:block w-[14.8%]  top-[47.1%] right-[34.9%] "
         src={shortlineteam}
         alt="line"
       />
       {/* long downward line */}
       <img
-        className="absolute hidden  md:max-2xl:block lg:block h-[9.4%]  top-[47.09%] right-[49.5%] "
+        className="absolute hidden   lg:block h-[9.4%]  top-[47.09%] right-[49.5%] "
         src={arrowteam273}
         alt="line"
       />
       {/* small downward arrow */}
       <img
-        className="absolute  hidden  md:max-2xl:block lg:block h-[3.5%]  top-[56.4%] right-[49%] "
+        className="absolute  hidden   lg:block h-[3.5%]  top-[56.4%] right-[49%] "
         src={arrshortteam}
         alt="line"
       />
       {/* down left line */}
       <img
-        className="absolute  hidden  md:max-2xl:block lg:block w-[30.2%]   top-[58%] left-[20%] "
+        className="absolute  hidden   lg:block w-[30.2%]   top-[58%] left-[20%] "
         src={lineteamsvg}
         alt="line"
       />
       {/* down right line */}
       <img
-        className="absolute  hidden  md:max-2xl:block lg:block w-[30.2%]  top-[58%] right-[20%] "
+        className="absolute  hidden   lg:block w-[30.2%]  top-[58%] right-[20%] "
         src={lineteamsvg}
         alt="line"
       />
       {/* down right downward line */}
       <img
-        className="absolute  hidden  md:max-2xl:block lg:block   top-[58%] right-[19.5%] "
+        className="absolute  hidden   lg:block   top-[58%] right-[19.5%] "
         src={arrowline152}
         alt="line"
       />
       {/* down left downward line */}
       <img
-        className="absolute  hidden  md:max-2xl:block lg:block   top-[58%] left-[19.9%] "
+        className="absolute  hidden   lg:block   top-[58%] left-[19.9%] "
         src={arrowline152}
         alt="line"
       />
