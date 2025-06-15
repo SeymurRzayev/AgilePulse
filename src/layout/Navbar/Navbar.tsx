324470094ecda7b002adf860dd9fe144b87a4865
Farmayil001 <gasanoff8085@gmail.com>
2025-06-15 16:44:44 +0400

    Log out

--- src/layout/Navbar/Navbar.tsx
+++ src/layout/Navbar/Navbar.tsx
@@ -4,8 +4,13 @@ import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
 import styles from "./Navbar.module.css";
 import logo from "../../assets/images/logoWithOutTitle.svg";
 import loginIcon768 from "../../assets/images/login-768.svg"
-import { useAppSelector } from "../../redux/hooks/Hooks";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks/Hooks";
 import loginAvatar from '../../assets/icons/loginAvatar.svg'
+import { IoIosArrowDown } from "react-icons/io";
+import { IoSettingsOutline } from "react-icons/io5";
+import { FiLogOut } from "react-icons/fi";
+import { userLogout } from "../../redux/slices/authSlice";
+import Swal from "sweetalert2";
 interface NavLinkItem {
   path: string;
   label: string;
@@ -27,8 +32,10 @@ const Navbar: FC<NavbarProps> = ({
 }) => {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   const navigate = useNavigate()
+  const dispatch = useAppDispatch()
   const location = useLocation();
   const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn)
+  const [openDropDown, setOpenDropDown] = useState<boolean>(false)
   // Navigation links configuration
   const navLinks: NavLinkItem[] = [
     ...(isLoggedIn && isMenuOpen
@@ -76,7 +83,22 @@ const Navbar: FC<NavbarProps> = ({
     };
   }, [isMenuOpen]);
 
-  // Handle scroll events to change navbar appearance
+  const handleLogOut = () => {
+    Swal.fire({
+      title: 'Çıxış etmək istədiyinizə əminsiniz?',
+      text: "Sessiyanız bağlanacaq.",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Bəli, çıxış et',
+      cancelButtonText: 'İmtina'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        dispatch(userLogout());
+      }
+    });
+  };
 
   // Close menu on route change
   useEffect(() => {
@@ -187,43 +209,86 @@ const Navbar: FC<NavbarProps> = ({
 
         {!logoOnly && (
           <div className={`${styles.actions}`}>
-            {isLoggedIn ? <img src={loginAvatar} alt="loginAvator" className="cursor-pointer" onClick={() => navigate("/personal-cabinet")} /> : (
-              <>
-                <div className=" hidden lg:flex gap-4">
-                  <NavLink
-                    to="/sign-up"
-                    className={({ isActive }) =>
-                      `${styles.button} ${styles.colorBtn} ${isActive ? styles.active : ""
-                      }`
-                    }
-                  >
-                    Qeydiyyat
-                  </NavLink>
-                  <NavLink
-                    to="/sign-in"
-                    className={({ isActive }) =>
-                      `${styles.button} ${styles.outlineBtn} ${isActive ? styles.active : ""
-                      }`
-                    }
-                  >
-                    Daxil ol
-                  </NavLink>
+            {isLoggedIn
+              ?
+              <div className="flex flex-col relative">
 
-                </div>
+                <button
+                  onClick={() => setOpenDropDown(prev => !prev)}
+                  className="bg-[#2C4B9B] rounded-4xl py-2 px-3 flex items-center gap-1 cursor-pointer"
+                >
+                  <span className="bg-white rounded-full">
+                    <img src={loginAvatar} alt="loginAvatar" className="cursor-pointer" />
+                  </span>
+                  {
+                    <span
+                      className={`transition-transform duration-300 ${openDropDown ? 'rotate-180' : ''
+                        }`}
+                    >
+                      <IoIosArrowDown color="white" size={20} />
+                    </span>
+                  }
 
+                </button>
 
-                <div className="flex lg:hidden ">
-                  <NavLink
-                    to="/sign-in"
-                    className={({ isActive }) =>
-                      ` ${isActive ? `${styles.active}` : ""
-                      }`
-                    }
-                  >
-                    <img src={loginIcon768} alt="" className="w-[35px]" />
-                  </NavLink>
-                </div></>
-            )}
+                <ul
+                  className={`
+                      absolute top-[50px] right-0 px-3 space-y-2 py-5 rounded-lg bg-amber-50 w-[250px]
+                      transition-all duration-300 ease-in-out
+                      transform 
+                      ${openDropDown ? 'opacity-100 translate-y-0 visible' : 'opacity-0 -translate-y-2 invisible'}
+                `}
+                >
+                  <li onClick={() => navigate("/personal-cabinet")} className="flex items-center gap-3 text-lg text-[#122041] font-[Corbel] cursor-pointer font-normal">
+                    <span className="bg-[#CCC] rounded-full p-2">
+                      <IoSettingsOutline size={25} />
+                    </span>
+                    Hesabım
+                  </li>
+                  <li onClick={handleLogOut} className="flex items-center gap-3 text-lg text-[#122041] font-[Corbel] cursor-pointer font-normal">
+                    <span className="bg-red-100 rounded-full p-2">
+                      <FiLogOut size={25} />
+                    </span>
+                    Çıxış
+                  </li>
+                </ul>
+              </div>
+              : (
+                <>
+                  <div className=" hidden lg:flex gap-4">
+                    <NavLink
+                      to="/sign-up"
+                      className={({ isActive }) =>
+                        `${styles.button} ${styles.colorBtn} ${isActive ? styles.active : ""
+                        }`
+                      }
+                    >
+                      Qeydiyyat
+                    </NavLink>
+                    <NavLink
+                      to="/sign-in"
+                      className={({ isActive }) =>
+                        `${styles.button} ${styles.outlineBtn} ${isActive ? styles.active : ""
+                        }`
+                      }
+                    >
+                      Daxil ol
+                    </NavLink>
+
+                  </div>
+
+                  <div className="flex lg:hidden ">
+                    <NavLink
+                      to="/sign-in"
+                      className={({ isActive }) =>
+                        ` ${isActive ? `${styles.active}` : ""
+                        }`
+                      }
+                    >
+                      <img src={loginIcon768} alt="" className="w-[35px]" />
+                    </NavLink>
+                  </div></>
+              )}
           </div>
         )
         }
