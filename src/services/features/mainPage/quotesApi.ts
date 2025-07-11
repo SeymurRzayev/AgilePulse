1de99b66c52f718f7400f6ab951ebd7a73622370
Farmayil001 <gasanoff8085@gmail.com>
2025-07-11 13:33:38 +0400

    Quotes admin panel

--- src/services/features/mainPage/quotesApi.ts
+++ src/services/features/mainPage/quotesApi.ts
@@ -1,6 +1,11 @@
 import type { GetRandomQuotesRes } from "../../../types/types";
 import { baseApi } from "../../api/baseApi";
 
+interface QuoteReq {
+    text: string;
+    author: string;
+}
+
 
 const quotesApi = baseApi.injectEndpoints({
     endpoints: builder => ({
@@ -10,7 +15,49 @@ const quotesApi = baseApi.injectEndpoints({
                 method: 'GET',
             }),
         }),
-    })
-})
 
-export const { useGetRandomQuotesQuery } = quotesApi;
+        getAllQuotes: builder.query<GetRandomQuotesRes[], void>({
+            query: () => ({
+                url: '/quotes/search',
+                method: 'GET',
+            }),
+            transformResponse: (response: any) => response.content,
+            providesTags: ['Quote'],
+        }),
+
+        createQuote: builder.mutation<GetRandomQuotesRes, QuoteReq>({
+            query: (quote) => ({
+                url: '/quotes/createQuote',
+                method: 'POST',
+                body: quote,
+            }),
+            invalidatesTags: ['Quote'],
+        }),
+
+        updateQuote: builder.mutation<GetRandomQuotesRes, { quote: QuoteReq; id: number }>({
+            query: ({ quote, id }) => ({
+                url: `/quotes/updateQuote/${id}`,
+                method: 'PUT',
+                body: quote,
+            }),
+            invalidatesTags: ['Quote'],
+        }),
+
+        deleteQuote: builder.mutation<void, number>({
+            query: (id) => ({
+                url: `/quotes/disable/${id}`,
+                method: 'PUT',
+            }),
+            invalidatesTags: ['Quote'],
+        }),
+    }),
+});
+
+
+export const {
+    useGetRandomQuotesQuery,
+    useGetAllQuotesQuery,
+    useCreateQuoteMutation,
+    useUpdateQuoteMutation,
+    useDeleteQuoteMutation,
+} = quotesApi;
