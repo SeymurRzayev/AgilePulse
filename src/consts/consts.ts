6dc03e21ba0aec5d30d7a8f18d3a26acceca2ad6
aytan97 <aytan.sh.aa@gmail.com>
2025-07-16 16:49:52 +0400

    Exam page is on the way

---
+++ src/consts/consts.ts
@@ -0,0 +1,51 @@
+export const sxWhitePagination = {
+    '& .MuiPaginationItem-root': {
+        color: 'white',
+        borderColor: 'white',
+
+    },
+    '& .Mui-selected': {
+        backgroundColor: 'white' + ' !important',
+        color: '#000',
+    },
+    '& .MuiPaginationItem-ellipsis': {
+        color: 'white',
+    },
+    '& .Mui-selected:hover': {
+        backgroundColor: 'white',
+    },
+    '& .MuiPaginationItem-previousNext': {
+        borderRadius: '50%', // rounded
+        backdropFilter: 'blur(6px)', // blur effect
+        backgroundColor: 'rgba(255, 255, 255, 0.1)', // for visible blur
+        color: 'white',
+
+    },
+    '& .MuiPaginationItem-previousNext:hover': {
+        backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    },
+    '& .Mui-disabled.MuiPaginationItem-previousNext': {
+        opacity: 0.3,
+        color: 'white',
+    }
+};
+
+
+
+export const examSample = [
+    { title: 'PSM 1 | imtahan', questionsCount: 60, duration: 60 },
+    { title: 'PSM 1 | imtahan', questionsCount: 60, duration: 60 },
+    { title: 'PSM 1 | imtahan', questionsCount: 60, duration: 60 },
+    { title: 'PSM 1 | imtahan', questionsCount: 60, duration: 60 },
+    { title: 'PSM 1 | imtahan', questionsCount: 60, duration: 60 },
+    { title: 'PSM 1 | imtahan', questionsCount: 60, duration: 60 },
+    { title: 'PSM 1 | imtahan', questionsCount: 60, duration: 60 },
+    { title: 'PSM 1 | imtahan', questionsCount: 60, duration: 60 },
+    { title: 'PSM 1 | imtahan', questionsCount: 60, duration: 60 },
+    { title: 'PSM 1 | imtahan', questionsCount: 60, duration: 60 },
+    { title: 'PSM 1 | imtahan', questionsCount: 60, duration: 60 },
+    { title: 'PSM 1 | imtahan', questionsCount: 60, duration: 60 },
+    { title: 'PSM 1 | imtahan', questionsCount: 60, duration: 60 },
+    { title: 'PSM 1 | imtahan', questionsCount: 60, duration: 60 },
+    // add more data...
+];
