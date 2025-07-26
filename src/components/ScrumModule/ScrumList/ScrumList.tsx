2f70f5f7fbd71b52a7f2ba4ec3f15a4a5add0deb
Sedi98 <sedi9816@gmail.com>
2025-07-26 19:20:34 +0400

    ready trainingReview module

--- src/components/ScrumModule/ScrumList/ScrumList.tsx
+++ src/components/ScrumModule/ScrumList/ScrumList.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Button from "../../../ui/Button/Button";
 import AccordionItem from "../../AccordionItem";
 import Slider from "react-slick";
@@ -6,14 +7,36 @@ import TrainingExperiences from "../../../pages/Home/sections/TrainingExperience
 import { useNavigate } from "react-router-dom";
 import type { Training } from "../../../types/types";
 import { useGetAllTrainingsQuery } from "../../../services/features/trainingPage/trainingsApi";
+import ScrumReview from "./ScrumReview";
 
 type ScrumListProps = {
   data: Training | undefined;
-}
+};
 
 const ScrumList = ({ data }: ScrumListProps) => {
+  const [comment, setComment] = useState("");
+  const [rating, setRating] = useState(0);
+  const [commentLimit] = useState(100);
 
-  const {data: allTrainings} = useGetAllTrainingsQuery()
+  const handleComment = (text: string) => {
+    if (comment.length >= commentLimit) {
+      setComment(text.slice(0, commentLimit));
+    } else {
+      setComment(text);
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>, id: number|undefined) => {
+    e.preventDefault();
+    console.log(comment, rating, id);
+    if (rating === 0) {
+      alert("Davam etmək üçün xahiş edirik qiymətləndirin");
+    } else if (comment.length === 0) {
+      alert("Davam etmək üçün xahiş edirik şərhinizi daxil edin");
+    }
+  };
+
+  const { data: allTrainings } = useGetAllTrainingsQuery();
 
   const settings = {
     dots: false,
@@ -30,26 +53,25 @@ const ScrumList = ({ data }: ScrumListProps) => {
           slidesToShow: 3,
           slidesToScroll: 1,
           infinite: true,
-          dots: false
-        }
+          dots: false,
+        },
       },
       {
         breakpoint: 924,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 1
-        }
+          slidesToScroll: 1,
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
+          slidesToScroll: 1,
+        },
       },
-
-    ]
-  }
+    ],
+  };
 
   const navigate = useNavigate();
   const handleClick = () => {
@@ -60,20 +82,33 @@ const ScrumList = ({ data }: ScrumListProps) => {
     <>
       <div className="w-[80%] mx-auto">
         <div className=" flex flex-col gap-[60px] font-corbel">
-          {
-            data?.modules?.map((mod) => (
-              <div key={mod.id} className="flex flex-col gap-y-[5px]">
-                <h2 className="text-2xl font-bold">{mod.title}</h2>
-                {mod.lessons.map((lesson) => (
-                  <AccordionItem key={lesson.id} moduleTitle={mod.title} lessonTitle={lesson.title} contentHtml={lesson.contentHtml} />
-                ))}
-              </div>
-            ))
-          }
+          {data?.modules?.map((mod) => (
+            <div key={mod.id} className="flex flex-col gap-y-[5px]">
+              <h2 className="text-2xl font-bold">{mod.title}</h2>
+              {mod.lessons.map((lesson) => (
+                <AccordionItem
+                  key={lesson.id}
+                  moduleTitle={mod.title}
+                  lessonTitle={lesson.title}
+                  contentHtml={lesson.contentHtml}
+                />
+              ))}
+            </div>
+          ))}
           <Button
             onClick={handleClick}
             title="Tapşırıq və quiz"
             className="self-center !font-medium !font-[Montserrat] bg-gradient-to-r from-[#4A00E0] to-[#8E2DE2]"
+          />
+
+          <ScrumReview
+            handleSubmit={handleSubmit}
+            reviewComment={comment}
+            setComment={handleComment}
+            reviewRating={rating}
+            setRating={setRating}
+            limit={commentLimit}
+            id={data?.id}
           />
         </div>
       </div>
@@ -82,32 +117,33 @@ const ScrumList = ({ data }: ScrumListProps) => {
         <h2 className="text-center">İstifadəçilər bu təlimləri də keçdilər </h2>
         <div className="w-full  gap-8 mx-auto pt-2 items-center flex-col md:flex-row ">
           <Slider {...settings} className="">
-            {
-              allTrainings?.map(item => (
-                <div key={item.id} className='px-3 py-3'>
-                  <TrainingCard
-                    id={item.id}
-                    isCurveBig={true}
-                    className={'w-[325px] md:w-[381px] h-[621px]'}
-                    imgUrl={item.imageUrl}
-                    date={item.publishedAt}
-                    avatar={item.authorAvatarUrl}
-                    title={item.title}
-                    lessonCount={item.modules?.reduce((total, mod) => total + mod.lessons.length, 0)}
-                    isArticle={false}
-                    user={item.authorName}
-                    isCourse={true}
-                    time={item.modules?.length}
-                  />
-                </div>
-              ))
-            }
+            {allTrainings?.map((item) => (
+              <div key={item.id} className="px-3 py-3">
+                <TrainingCard
+                  id={item.id}
+                  isCurveBig={true}
+                  className={"w-[325px] md:w-[381px] h-[621px]"}
+                  imgUrl={item.imageUrl}
+                  date={item.publishedAt}
+                  avatar={item.authorAvatarUrl}
+                  title={item.title}
+                  lessonCount={item.modules?.reduce(
+                    (total, mod) => total + mod.lessons.length,
+                    0
+                  )}
+                  isArticle={false}
+                  user={item.authorName}
+                  isCourse={true}
+                  time={item.modules?.length}
+                />
+              </div>
+            ))}
           </Slider>
         </div>
       </div>
+
       <TrainingExperiences />
     </>
-
   );
 };
 
