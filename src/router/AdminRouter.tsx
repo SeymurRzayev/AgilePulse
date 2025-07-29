6eabe967535480b3e2f319d84e8fb58af48bf4e5
Farmayil001 <gasanoff8085@gmail.com>
2025-07-29 12:12:40 +0400

    Refactor admin routing and authentication: streamline App component, enhance AdminMenu with logout confirmation, and update ProtectedRoute for role-based access control.

--- src/router/AdminRouter.tsx
+++ src/router/AdminRouter.tsx
@@ -18,36 +18,33 @@ import AdminQuizs from "../components/Admin/Trainings/AdminQuizs";
 import AdminCourses from "../components/Admin/Trainings/AdminCourses";
 import AdminAbout from "../components/Admin/AboutPage/AdminAbout";
 import AdminFaq from "../components/Admin/HeaderFooterPage/AdminFaq";
-import ProtectedRoute from "./ProtectedRoute"
 
 const AdminRouter = () => {
     return (
         <Routes>
-            <Route element={<ProtectedRoute allowedRoles={['ADMIN']} />}>
-                <Route path="*" element={<AdminPanel />}>
-                    <Route path="dashboard">
-                        <Route index element={<Dashboard />} />
-                        <Route path="main-page" element={<AdminSections pageType={AdminPageType.Main} />} />
-                        <Route path="main-page/library" element={<AdminLibraryAndArticles />} />
-                        <Route path="main-page/quotes" element={<AdminQuotes />} />
-                        <Route path="main-page/articles" element={<AdminLibraryAndArticles />} />
-                        <Route path="main-page/partners" element={<AdminPartners />} />
-                        <Route path="header-footer" element={<AdminSections pageType={AdminPageType.HeaderAndFooter} />} />
-                        <Route path="header-footer/faq" element={<AdminFaq />} />
-                        <Route path="trainings-page" element={<AdminSections pageType={AdminPageType.Trainings} />} />
-                        <Route path="trainings-page/categories" element={<AdminCategories />} />
-                        <Route path="trainings-page/trainers" element={<AdminTrainers />} />
-                        <Route path="trainings-page/podcasts" element={<AdminPodcasts />} />
-                        <Route path="trainings-page/quizs" element={<AdminQuizs />} />
-                        <Route path="trainings-page/courses" element={<AdminCourses />} />
-                        <Route path="about-page" element={<AdminAbout />} />
-                    </Route>
-                    <Route path="users" element={<Users />} />
-                    <Route path="projects" element={<Projects />} />
-                    <Route path="tasks" element={<Tasks />} />
-                    <Route path="groups" element={<Groups />} />
-                    <Route path="notificationSystem" element={<NotificationCenter />} />
+            <Route path="/" element={<AdminPanel />}>
+                <Route path="dashboard">
+                    <Route index element={<Dashboard />} />
+                    <Route path="main-page" element={<AdminSections pageType={AdminPageType.Main} />} />
+                    <Route path="main-page/library" element={<AdminLibraryAndArticles />} />
+                    <Route path="main-page/quotes" element={<AdminQuotes />} />
+                    <Route path="main-page/articles" element={<AdminLibraryAndArticles />} />
+                    <Route path="main-page/partners" element={<AdminPartners />} />
+                    <Route path="header-footer" element={<AdminSections pageType={AdminPageType.HeaderAndFooter} />} />
+                    <Route path="header-footer/faq" element={<AdminFaq />} />
+                    <Route path="trainings-page" element={<AdminSections pageType={AdminPageType.Trainings} />} />
+                    <Route path="trainings-page/categories" element={<AdminCategories />} />
+                    <Route path="trainings-page/trainers" element={<AdminTrainers />} />
+                    <Route path="trainings-page/podcasts" element={<AdminPodcasts />} />
+                    <Route path="trainings-page/quizs" element={<AdminQuizs />} />
+                    <Route path="trainings-page/courses" element={<AdminCourses />} />
+                    <Route path="about-page" element={<AdminAbout />} />
                 </Route>
+                <Route path="users" element={<Users />} />
+                <Route path="projects" element={<Projects />} />
+                <Route path="tasks" element={<Tasks />} />
+                <Route path="groups" element={<Groups />} />
+                <Route path="notificationSystem" element={<NotificationCenter />} />
             </Route>
         </Routes>
     );
